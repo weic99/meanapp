bb46a43e4723032676d2c0dbc588a80570ae113f
Wei Chen <tengwei1@gmail.com>
2017-07-26 11:48:09 -0400

    fixed paths

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 
-const User = require('..models/user');
+const User = require('../models/user');
 
 // Register page
 router.get('/register', (req, res, next) => {
