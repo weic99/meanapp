ca2e75182b02dde1e502c7745d42e86e13c8447a
Wei Chen <tengwei1@gmail.com>
2017-07-27 00:17:38 -0400

    added flash messages

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
 import { ValidateService } from './services/validate.service';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 const appRoutes: Routes = [
    {path: '', component: HomeComponent},
@@ -36,7 +37,8 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule
   ],
   providers: [ValidateService],
   bootstrap: [AppComponent]
--- angular/src/app/components/register/register.component.ts
+++ angular/src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +13,8 @@ export class RegisterComponent implements OnInit {
   email: String;
   password: String;
 
-  constructor(private validateService: ValidateService) { }
+  constructor(private validateService: ValidateService,
+              private flashMessages: FlashMessagesService) { }
 
   ngOnInit() {}
 
@@ -26,12 +28,18 @@ export class RegisterComponent implements OnInit {
 
     // ValidateService
     if (!this.validateService.validateRegister(user)) {
-      console.log('please');
+      this.flashMessages.show('Please fill in all fields', {
+        cssClass: 'alert-danger',
+        timeout: 3000
+      });
       return false;
     }
 
     if (!this.validateService.validateEmail(user.email)) {
-      console.log('email suks');
+      this.flashMessages.show('Please use a valid email', {
+        cssClass: 'alert-danger',
+        timeout: 3000
+      });
       return false;
     }
 
