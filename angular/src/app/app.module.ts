9af7533441fd03fc8eb6daf9a9c516310b836625
Wei Chen <tengwei1@gmail.com>
2017-07-29 13:44:18 -0400

    MEAN stack login app

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -22,7 +22,8 @@ const appRoutes: Routes = [
    {path: 'register', component: RegisterComponent},
    {path: 'login', component: LoginComponent},
    {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
-   {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]}
+   {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
+   {path: '**', component: HomeComponent}
 ];
 
 @NgModule({
--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.get('/', (req, res) => {
 app.use('/users', users);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.send('hello');
 });
 
 // Start server
